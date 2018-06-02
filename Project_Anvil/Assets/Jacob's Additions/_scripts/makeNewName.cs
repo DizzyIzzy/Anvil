38e9d9023b385e3c0a3213bf3a1cd45cb8bf6215
ehorwitz1 <jacob.c.claytor@gmail.com>
2018-06-02 16:24:23 -0600

    NPC Name Generator
    Added clean version of Last Names to resources that doesn't have the extra data. Created static class makeNewName that can call generate() in NPC object scripts to give each one a unique name.

---
+++ Project_Anvil/Assets/Jacob's Additions/_scripts/makeNewName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+// This will be used to generate names for NPC's. In the NPC's start() function, we can simply call: 'string myName = makeNewName.generate();'
+public static class makeNewName : object {
+
+	// Creates array of all last names in the lastName.txt file
+	static string[] lines = System.IO.File.ReadAllLines(@"Assets\Resources\lastNamesCleaned.txt");
+	// String of all chars that will be used for the First Name Initial and Middle Name Initial
+	static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	// Generates the randomized name
+	public static string generateName(){
+
+		int numLast = Random.Range (0, lines.Length - 1);
+		int numFirst = Random.Range (0, 25);
+		int numMiddle = Random.Range (0, 25);
+
+		// Returns last name in proper format: 'Smith, A.B.'
+		return lines[numLast] + ", " + alphabet[numFirst] + "." + alphabet[numMiddle] + ".";
+	}
+
+}
